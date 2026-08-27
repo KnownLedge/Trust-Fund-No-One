a6d7580d404ff3f41a75be73fa53cbf5fdabadeb
KnownLedge <knowlegekeeper360@gmail.com>
2026-08-28 00:02:23 +0100

    Picking up papers
    You can now pick up papers, stretch and retract your arm, and then drop them by pressing Q
    yes i know you can pick up the map aswell right now im working on it
    oh also we're back on 4.7 now

--- trust-fund-no-one/Scripts/NodeScripts/player_controller.gd
+++ trust-fund-no-one/Scripts/NodeScripts/player_controller.gd
@@ -5,8 +5,32 @@ extends Node3D
 
 const SENSITIVITY = 0.005
 
+const HAND_RETURN = 3
+
+const HAND_EXTEND = 2
+
+const LEFT_TURN = 7.5
+
+@export var left_hand: Node3D
+
+@export var ray: RayCast3D
+
+@export var default_left_hold: Vector3
+
+var left_item: Node3D
+
+var left_hold_pos: Vector3
+
+var left_hover_pos: Vector3
+
+
+
+enum holdType {PAPER, STAPLER}
+var left_hold_type: holdType
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	left_hover_pos = left_hand.position
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -20,3 +44,49 @@ func _process(delta: float) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	elif(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+	
+	if(Input.is_action_just_pressed("LeftDrop")):
+		left_item.reparent(get_parent())
+		if(left_item is RigidBody3D):
+			left_item.freeze = false
+			
+		left_item = null
+		left_hand.position = left_hover_pos
+
+
+
+func _physics_process(delta: float) -> void:
+	if(Input.is_action_just_pressed("LeftClick")):
+		if(left_item == null and ray.is_colliding()):
+			print("picking up thing")
+			left_hand.global_position = ray.get_collision_point()
+			left_hold_pos = left_hand.position
+			if(true):#ray.get_collider() is CSGBox3D):
+				print(ray.get_collider().name)
+				left_item = ray.get_collider()
+			else:
+				print(ray.get_collider().get_parent_node().name)
+				left_item = ray.get_collider().get_parent_node()
+			if(left_item is RigidBody3D):
+				left_item.freeze = true
+			
+
+			
+			
+			left_item.reparent(left_hand)
+			
+			if(left_item.get_meta("hold_rotation")):
+				left_item.rotation_degrees = left_item.get_meta("hold_rotation")
+			
+			left_item.rotate_y(deg_to_rad(LEFT_TURN))
+			
+			
+		else:
+			print("not picking thing up")
+	elif(Input.is_action_pressed("LeftClick")):#checking for held input
+		left_hand.position = lerp(left_hand.position, left_hold_pos, delta * HAND_EXTEND)
+	elif(left_item != null): 
+		left_hand.position = lerp(left_hand.position, left_hover_pos, delta * HAND_RETURN)
+		if(left_hand.position.distance_to(left_hover_pos) < 2):
+			left_hold_pos = default_left_hold
