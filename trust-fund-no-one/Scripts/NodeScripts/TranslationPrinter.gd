015fc299fc5e86fb45b6c57a01b08df6b2185656
KnownLedge <knowlegekeeper360@gmail.com>
2026-08-30 04:36:31 +0100

    Extra page printing
    if page yaps on too long, another page gets printed to continue the tale
    
    this was oddly annoying to do

--- trust-fund-no-one/Scripts/NodeScripts/TranslationPrinter.gd
+++ trust-fund-no-one/Scripts/NodeScripts/TranslationPrinter.gd
@@ -10,6 +10,8 @@ extends Node
 
 @export var paper_eject_force: Vector3
 
+@export var char_limit = 450
+
 var text_to_print:String
 
 var current_paper: Node3D
@@ -20,11 +22,14 @@ var is_active: bool = false
 
 var string_pos = 0
 
+var cutoff_call_prog = 0
+
 func start_translation(printedText):
 	if(current_paper != null):
 		eject_paper()
 	print("translating")
-	text_to_print = printedText
+	if(printedText != null):
+		text_to_print = printedText
 	current_paper = paper_obj.duplicate()
 	get_parent().add_child(current_paper)
 	current_paper.position = paper_spawn_pos
@@ -36,16 +41,30 @@ func start_translation(printedText):
 func _process(delta: float) -> void:
 	if(is_active):
 		current_paper.position = lerp(current_paper.position, paper_print_pos, 6 * delta)
-		while(call_manager.call_progress > get_page_progress() and string_pos < text_to_print.length()):
+		while((call_manager.call_progress - cutoff_call_prog) > get_page_progress() and string_pos < text_to_print.length()):
 			current_paper_text.text = current_paper_text.text.insert(string_pos, text_to_print[string_pos])
 			#print("inserted text")
 			string_pos = string_pos + 1
 		
+		
 		if(call_manager.call_progress == 0 and string_pos > 0):
 			is_active = false
 			eject_paper()
-		
+		elif(current_paper_text.text.length() > char_limit):
+			safe_eject_paper()
+			#text_to_print = text_to_print.substr(string_pos)
+			cutoff_call_prog = call_manager.call_progress
+			
+			start_translation(null)
 		#print("call progress " + str(call_manager.call_progress) + "print progress " + str(get_page_progress()))
+
+func safe_eject_paper():
+	current_paper.position = paper_print_pos
+	current_paper.get_node("CollisionShape3D").disabled = false
+	current_paper.freeze = false
+	current_paper.apply_central_force(paper_eject_force)
+	current_paper = null
+	current_paper_text = null
 
 func eject_paper():
 	string_pos = 0
@@ -55,6 +74,7 @@ func eject_paper():
 	current_paper.apply_central_force(paper_eject_force)
 	current_paper = null
 	current_paper_text = null
+	cutoff_call_prog = 0
 
 func get_page_progress():
 	return float(current_paper_text.text.length()) / float(text_to_print.length())
